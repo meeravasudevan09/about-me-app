83435c44c72ac1f34e5b441f1ca8b731eb4b3b63
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-11-15 12:28:26 +1100

    Initial commit

---
+++ app/src/main/java/com/example/meeravasudevan/aboutme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.meeravasudevan.aboutme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
